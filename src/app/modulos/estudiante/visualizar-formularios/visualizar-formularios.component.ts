6a3ce6272b48291d967b573e64bb89ba6dfd9b1f
Diego <alejandrochavz@outlook.es>
2020-11-30 09:14:53 -0500

    Visualización de formularios

--- src/app/modulos/estudiante/visualizar-formularios/visualizar-formularios.component.ts
+++ src/app/modulos/estudiante/visualizar-formularios/visualizar-formularios.component.ts
@@ -3,6 +3,12 @@ import {FormularioServiceImpl} from "../../../core/http/implement/formulario.ser
 import Formulario from "../../../core/models/formulario.model";
 import FormularioResponse from "../../../core/models/formulario_response.model";
 import {Router} from "@angular/router";
+import Docente from "../../../core/models/docente.model";
+import DocenteResponse from "../../../core/models/docente_response.model";
+import Programa from "../../../core/models/programa.model";
+import {DocenteServiceImpl} from "../../../core/http/implement/docente.service.impl";
+import {ProgramaServiceImpl} from "../../../core/http/implement/programa.service.impl";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 interface Food {
   value: string;
@@ -19,37 +25,104 @@ export class VisualizarFormulariosComponent implements OnInit {
 
   listFormularios: FormularioResponse[];
 
+  listDocentes: DocenteResponse[];
+
+  listProgramas: Programa[];
+
   loading: boolean;
 
+  formInicio: FormGroup;
 
   test: Date = null;
-
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Item 1'},
-    {value: 'pizza-1', viewValue: 'Item 2'},
-    {value: 'tacos-2', viewValue: 'Item 3'}
-  ];
 
   focus: any;
   focus1: any;
 
+  listAux: FormularioResponse[];
+
   constructor(private formularioService: FormularioServiceImpl,
+              private docenteService: DocenteServiceImpl,
+              private programaService: ProgramaServiceImpl,
               private routes: Router,
-              ) {
+              private _formBuilder: FormBuilder,
+  ) {
     this.listFormularios = [];
+    this.listDocentes = [];
+    this.listProgramas = [];
     this.loading = true;
   }
 
   ngOnInit(): void {
+    this.crearForm();
     this.test = new Date();
     this.getListFormulario();
+    this.getDocentes();
+    this.getProgramas();
+  }
+
+  crearForm() {
+    this.formInicio = this._formBuilder.group({
+      docente: null,
+    });
+
+    this.formInicio.get('docente').valueChanges.subscribe(res => {
+      this.filtrarPorDocente(res);
+    })
+  }
+
+
+  filtrarPorDocente(idDocente) {
+    this.listFormularios = this.listAux;
+    if (this.listFormularios.length > 0) {
+      let aux = [];
+      for (let i of this.listFormularios) {
+        if (i.docente == idDocente) {
+          aux.push(i);
+        }
+      }
+      this.listFormularios = aux;
+    }
+  }
+
+  getDocentes() {
+    this.loading = false;
+    this.docenteService.getAll().subscribe((res: DocenteResponse[]) => {
+      this.listDocentes = res;
+    }, () => {
+      console.log("ERROR AL CARGA FORMULARIO EN PRINCIPAL")
+    }, () => {
+      this.loading = true;
+    })
+  }
+
+
+  getProgramas() {
+    this.loading = false;
+    this.programaService.getAll().subscribe((res: Programa[]) => {
+      this.listProgramas = res;
+    }, () => {
+      console.log("ERROR AL CARGA FORMULARIO EN PRINCIPAL")
+    }, () => {
+      this.loading = true;
+    })
+  }
+
+  clearParametros(){
+    this.listFormularios = this.listAux;
   }
 
   getListFormulario() {
+    this.listFormularios = [];
     this.loading = false;
-    this.formularioService.getAll().subscribe(res => {
-      this.listFormularios = res;
-      console.log(this.listFormularios)
+    this.formularioService.getAll().subscribe((res: FormularioResponse[]) => {
+      let aux = []
+      for (let i of res) {
+        if (i.activo == 1) {
+          aux.push(i)
+        }
+      }
+      this.listFormularios = aux;
+      this.listAux = aux;
     }, () => {
       console.log("ERROR AL CARGA FORMULARIO EN PRINCIPAL")
     }, () => {
@@ -58,6 +131,6 @@ export class VisualizarFormulariosComponent implements OnInit {
   }
 
   irFormulario(enlace) {
-    document.location.href = 'http://localhost:4200/#/formulario/'+enlace;
+    document.location.href = 'http://localhost:4200/#/formulario/' + enlace;
   }
 }
