0b044a1378a6630eb38cdbdd0b86596f8d4b567a
Diego <alejandrochavz@outlook.es>
2020-12-01 18:08:40 -0500

    URL DOCUMENTO

--- src/app/core/constants/url_formulario.constants.ts
+++ src/app/core/constants/url_formulario.constants.ts
@@ -1,4 +1,11 @@
 export const URL_FORMULARIO = {
   BASE: 'http://agendadoc.cpsw.ingsistemasufps.co/#/formulario/',
+}
+
+export const URL_DOCUMENTO = {
+  BASE_DOCUMENTO: 'http://agendadocb.cpsw.ingsistemasufps.co/media/solicitudes/',
+}
+
+export const URL_CALIFICACION = {
   BASE_ASESORIA: 'http://agendadoc.cpsw.ingsistemasufps.co/#/calificacion/',
 }
